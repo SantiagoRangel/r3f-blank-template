0c309a53556f398f4c6f466754afb9bbef39cc9b
SantiagoRangel <s.rangel@uniandes.edu.co>
2023-11-07 15:16:56 +0100

    first commit

---
+++ src/Experience.tsx
@@ -0,0 +1,11 @@
+import { OrbitControls } from "@react-three/drei"
+import Globe from "./Globe"
+
+export default function Experience() {
+  return (
+    <>
+      <OrbitControls />
+      <Globe />
+    </>
+  )
+}
---
+++ src/Globe.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Globe() {
+  return (
+    <mesh>
+      <boxGeometry />
+      <meshStandardMaterial color={"white"} />
+    </mesh>
+  )
+}
